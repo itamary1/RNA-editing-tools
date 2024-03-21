943b849b80c4e38ed7ea4f4a2e835746e7baab8e
itamary1 <itamarty@gmail.com>
2024-03-21 14:55:43 +0200

    typo

--- Substructures_Calculator/usage_exmple.sh
+++ Substructures_Calculator/usage_exmple.sh
@@ -2,10 +2,10 @@
 script_dir=$(dirname ${0})
 genome=${script_dir}"/first10000_sacCer3.fa"
 editing_sites_table=${script_dir}"/editing_sites.csv"
-data_tabels="RNAstructure/data_tables/"
+data_tables="RNAstructure/data_tables/"
 FOLD="Fold"
 bpRNA="bpRNA.pl"
 out_table_path=${script_dir}"/editing_sites_with_substructurs.csv"
 
-python Substructures_Calculator.py $genome $editing_sites_table $data_tabels $FOLD $bpRNA $out_table_path
+python Substructures_Calculator.py $genome $editing_sites_table $data_tables $FOLD $bpRNA $out_table_path
 
